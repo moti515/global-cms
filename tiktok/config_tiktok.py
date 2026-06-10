a19ccb1df005cf7ecbd4885524463f4941e4866e
moti515 <motia@ua.fm>
2026-06-10 18:23:07 +0200

    Update config_tiktok.py

--- tiktok/config_tiktok.py
+++ tiktok/config_tiktok.py
@@ -14,10 +14,11 @@ TOKENS_FILE = 'tiktok_tokens.json'
 FOLDER_INPUT_ID = '19wPAbTuyGGqMI4twWXfU5gfs-vk2Ru_G'
 FOLDER_TRASH_ID = '1L3veD90e7Fr1acwlK7PmhSs_JrofyT6N'
 
-TARGET_DURATION = 8  
 FINAL_FPS = 30        
 
-MUSIC_FALLBACK_PATH = 'assets/trending_travel_music.mp3'
+# Вказуємо шлях до папки з музикою. 
+# Оскільки ви робите `cd tiktok`, скрипт шукатиме папку 'music' прямо в ньому.
+MUSIC_FALLBACK_PATH = 'music'
 
 # Підтримувані формати медіа
 VALID_EXTENSIONS = (
